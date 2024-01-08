cc02c30a83c14d87c29d1f963e62563ab804b573
heathk1iff <heathk1iff@outlook.com>
2024-01-08 13:13:20 +0300

    Added sample Observer pattern

---
+++ Samples.Observer/ObservableValue.cs
@@ -0,0 +1,60 @@
+﻿namespace Samples.Observer
+{
+    public class ObservableValue : IObservable<ObservableValue>
+    {
+        private int _value;
+        private HashSet<IObserver<ObservableValue>> _observers = new();
+
+        public int CurrentValue 
+        {
+            get => _value;
+            set
+            {
+                bool isChanged = _value != value;
+                _value = value;
+                
+                if (isChanged)
+                {
+                    NotifyAll();
+                }
+            }
+        }
+
+        private void NotifyAll()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.OnNext(this);
+            }
+        }
+
+
+        public IDisposable Subscribe(IObserver<ObservableValue> observer)
+        {
+            if (!_observers.Contains(observer))
+            {
+                _observers.Add(observer);
+            }
+
+            return new Unsubscriber(_observers, observer);
+        }
+
+        private class Unsubscriber : IDisposable
+        {
+            HashSet<IObserver<ObservableValue>> _observers;
+            IObserver<ObservableValue> _observer;
+
+            public Unsubscriber(HashSet<IObserver<ObservableValue>> observers, IObserver<ObservableValue> observer) 
+            { 
+                _observer = observer;
+                _observers = observers;
+            }
+
+            public void Dispose()
+            {
+                _observers.Remove(_observer);
+                _observer = null;
+            }
+        }
+    }
+}
---
+++ Samples.Tests/Patterns/ObservableValueTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using Samples.Observer;
+
+namespace Samples.Tests.Patterns
+{
+    [TestFixture()]
+    public class ObservableValueTests
+    {
+        [Test()]
+        public void SubscribeTest()
+        {
+            var observer = new Mock<IObserver<ObservableValue>>();
+            observer.Setup(f => f.OnNext(It.IsAny<ObservableValue>())).Verifiable();
+            var obj = new ObservableValue();
+            obj.Subscribe(observer.Object);
+
+            obj.CurrentValue = 1;
+
+            observer.Verify();
+        }
+    }
+}
