3d09f132497e62d6952022367ad26e3aa4975ea8
heathk1iff <heathk1iff@outlook.com>
2024-01-08 17:04:54 +0300

    Added sample Facade pattern

---
+++ Samples.Facade/JsonFacade.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace Samples.Facade
+{
+    public abstract class JsonFacade
+    {
+        public static string ToJson<T>(T obj)
+        {
+           return JsonSerializer.Serialize<T>(obj);
+        }
+
+        public static T? ToObject<T>(string jsonData)
+        {
+            return (T?) JsonSerializer.Deserialize<T>(jsonData);
+        }
+    }
+}
---
+++ Samples.Tests/Patterns/JsonFacadeTests.cs
@@ -0,0 +1,62 @@
+﻿using Samples.Facade;
+
+namespace Samples.Tests.Patterns
+{
+    [TestFixture()]
+    public class JsonFacadeTests
+    {
+        [Test()]
+        public void ToJsonTest()
+        {
+            string expected = "{\"Id\":10}";
+
+            string actual = JsonFacade.ToJson(new JsonObject(10));
+
+            Assert.That(actual.Equals(expected), Is.True);
+        }
+
+        [Test()]
+        public void ToObjectTest()
+        {
+            JsonObject expected = new JsonObject(10);
+            JsonObject actual = JsonFacade.ToObject<JsonObject>("{\"Id\":10}");
+
+            Assert.That(actual.Equals(expected), Is.True);
+        }
+
+        private class JsonObject : IEquatable<JsonObject?>
+        {
+            public JsonObject(int id)
+            {
+                Id = id;
+            }
+            public int Id { get; }
+
+            public override bool Equals(object? obj)
+            {
+                return Equals(obj as JsonObject);
+            }
+
+            public bool Equals(JsonObject? other)
+            {
+                return other is not null &&
+                       Id == other.Id;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(Id);
+            }
+
+            public static bool operator ==(JsonObject? left, JsonObject? right)
+            {
+                return EqualityComparer<JsonObject>.Default.Equals(left, right);
+            }
+
+            public static bool operator !=(JsonObject? left, JsonObject? right)
+            {
+                return !(left == right);
+            }
+        }
+    }
+}
