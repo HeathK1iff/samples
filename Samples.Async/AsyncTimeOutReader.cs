1550763965f7633985fb88c38b34fac79b1619d3
heathk1iff <heathk1iff@outlook.com>
2024-02-04 22:43:32 +0300

    Added sample with function some work, and possibility to set timeout. if work time > timeout then throw exception

---
+++ Samples.Async/AsyncTimeOutReader.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Samples.Async
+{
+    public class AsyncTimeOutReader
+    {
+        private const int TimeOfWork = 10000;
+
+        public async Task<int> SomeWork10SecAsync(int millisecondsReadTimeOut)
+        {
+            using CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(millisecondsReadTimeOut);
+            Task<int> task = CreateTaskWithSomeWork(999);
+            Task timeOutTask = Task.Delay(Timeout.InfiniteTimeSpan, cancellationTokenSource.Token);
+
+            Task result = await Task.WhenAny(task, timeOutTask);
+
+            if (result == timeOutTask)
+                throw new TimeoutException();
+
+            return await task;
+        }
+
+        private Task<int> CreateTaskWithSomeWork(int returnValue)
+        {
+            return Task<int>.Factory.StartNew(() =>
+            {
+                Thread.Sleep(TimeOfWork);
+                return returnValue;
+            });
+        }
+
+    }
+}
---
+++ Samples.Tests/Samples/AsyncSamplesTests.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using Samples.Async;
+using System.Diagnostics;
+
+namespace Samples.Tests.Async
+{
+    [TestFixture]
+    public class AsyncSamplesTests
+    {
+        [Test]
+        public void ReadAsync_ReadWith6SecReadTimeOut_True()
+        {
+            var reader = new AsyncTimeOutReader();
+            Assert.Throws<TimeoutException>(() => {
+
+                try
+                {
+                    int result = reader.SomeWork10SecAsync((int) TimeSpan.FromSeconds(6).TotalMilliseconds).Result;
+                } catch (AggregateException ex)
+                {
+                    throw ex.InnerException ?? new Exception();
+                }
+                
+            });
+        }
+        [Test]
+        public void ReadAsync_ReadWith11SecReadTimeOut_True()
+        {
+            var reader = new AsyncTimeOutReader();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start(); 
+            int value = reader.SomeWork10SecAsync((int) TimeSpan.FromSeconds(12).TotalMilliseconds).Result;
+            sw.Stop();
+
+            Assert.IsTrue(value == 999);
+            Assert.IsTrue(sw.ElapsedMilliseconds > TimeSpan.FromSeconds(10).Milliseconds);
+        }
+
+    }
+}
