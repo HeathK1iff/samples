9676d4b7159e406a63eaf2e1cd25d7bd80fb4acb
heathk1iff <heathk1iff@outlook.com>
2024-02-21 21:38:06 +0300

    Added sample work with check progress from async method

---
+++ Samples.Async.Progress/AsyncProgress.cs
@@ -0,0 +1,17 @@
+﻿namespace Samples.Async.Progress
+{
+    public class AsyncProgress
+    {
+        public async Task LongWork(IProgress<int> progress, CancellationToken cancellationToken)
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i <= 10; i++)
+                {
+                    Task.Delay(1000);
+                    progress.Report(i * 10);
+                }
+            }, cancellationToken);
+        }
+    }
+}
---
+++ Samples.Tests/AsyncProgressTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using Samples.Async.Progress;
+
+namespace Samples.Tests.Async
+{
+    [TestFixture]
+    public class AsyncProgressTests
+    {
+        [Test]
+        public async Task LongWork_CheckProgress_True()
+        {
+            int actual = 0;
+            const int Expected = 100;
+            var target = new AsyncProgress();
+            CancellationTokenSource source = new CancellationTokenSource();
+            CancellationToken token = source.Token;
+            var progress = new Mock<IProgress<int>>();
+            progress.Setup(f => f.Report(It.IsAny<int>())).Callback<int>(f => {
+                actual = f;
+            });
+
+            await target.LongWork(progress.Object, token);
+
+            Assert.AreEqual(Expected, actual);
+        }
+    }
+}
