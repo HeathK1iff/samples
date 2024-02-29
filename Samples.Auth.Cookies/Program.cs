4173bbad711e6eb99a97a2b14a9b681f5a454d4e
heathk1iff <heathk1iff@outlook.com>
2024-02-29 18:58:18 +0300

    Added sample of authentication by Cookie and Jwt with autorize by role

---
+++ Samples.Auth.Cookies/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddConsole();
+
+builder.Services.AddControllersWithViews();
+builder.Services
+    .AddScoped<IUserValidatorService, UserValidatorService>()
+    .AddAuthorization(options =>
+    {
+        options.AddPolicy("AdministratorRole",
+            policy => policy.RequireRole("AdministratorX"));
+        options.AddPolicy("UserRole",
+            policy => policy.RequireRole("SimpleUser"));
+    })
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.ClaimsIssuer = "test";
+        options.LoginPath = "/xx";// Default: /Account/Login?ReturnUrl=%2F
+        options.ReturnUrlParameter = "";
+        options.Cookie.Name = "my_cookie";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+        options.SlidingExpiration = true;
+        options.AccessDeniedPath = "/Forbidden/";
+        options.Events = new CookieAuthenticationEvents()
+        {
+
+            OnValidatePrincipal = async (context) => {
+                await Task.Run<Task>(async () => {  
+                    Claim? userName = context.Principal?.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Name);
+            
+                    if (userName is null)
+                    {   
+                        context.RejectPrincipal();
+                        return;
+                    }
+
+                    var userValidatorService = context.HttpContext.RequestServices.GetRequiredService<IUserValidatorService>();
+
+                    if (!userValidatorService.IsAcceptedUser(userName.Value)){
+                        context.RejectPrincipal();  
+                        return;
+                    } 
+                });
+            }
+        };
+    });
+
+var app = builder.Build();
+app.UseCookiePolicy();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/login", async (HttpContext context) => {
+
+    var service = context.RequestServices.GetRequiredService<IAuthenticationService>();
+
+    var claims = new List<Claim>
+    {
+        new Claim(ClaimTypes.Name, "test@mail.ru"),
+        new Claim(ClaimTypes.Role, "Administrator"),
+        new Claim("newToken", "My-API-token")
+    };
+
+    var claimsIdentity = new ClaimsIdentity(
+        claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+    var authProperties = new AuthenticationProperties
+    {
+        RedirectUri = "/login2" // Redirect if success auth
+    };
+
+    await service.SignInAsync(context, CookieAuthenticationDefaults.AuthenticationScheme, 
+        new ClaimsPrincipal(claimsIdentity), authProperties);
+});
+
+app.MapGet("/admin", async (HttpContext context) => {
+    await context.Response.WriteAsync("test2");
+}).RequireAuthorization(f=>
+    {
+        f.RequireAuthenticatedUser();
+        f.RequireRole("AdministratorX");
+    });
+
+app.Run();
+
+public interface IUserValidatorService
+{
+    bool IsAcceptedUser(string userName);
+}
+
+public class UserValidatorService: IUserValidatorService
+{
+    private static User[] _users = new[] { new User(id: 1, name: "login1"), new User(id: 2, name: "test@mail.ru") };
+
+    public User[] GetAll(){
+        return _users;
+    }
+
+    public bool IsAcceptedUser(string name)
+    {
+        return GetAll().Any(user=>user.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+    }
+}
+
+public class User
+{
+    public User(int id, string name){
+        Id = id;
+        Name = name;
+    }
+
+    public int Id { get; }
+    public string Name { get; }
+}
---
+++ Samples.Auth.Jwt/Program.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+string jwtToken = string.Empty;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthorization();
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = "Issuer",
+            ValidateAudience = true,
+            ValidAudience = "services",
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("mysupersecret_secretsecretsecretkey!123")),
+            ValidateIssuerSigningKey = true
+         };
+
+        options.Events = new JwtBearerEvents(){
+            OnTokenValidated = (context) => {
+                Console.WriteLine("OnValidated");
+                
+                return Task.CompletedTask;
+            },
+            OnForbidden = async (context)=>{
+                await context.HttpContext.ForbidAsync();
+            }
+        };
+
+    });
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/", () => "Hello World");
+
+app.MapGet("/test", async (context) => { 
+    Console.WriteLine("TEST");
+    await context.Response.WriteAsync("test"); 
+    
+    }).RequireAuthorization();
+
+app.MapGet("/login", (HttpContext context) => {
+    Console.WriteLine("Creating token");
+    var service = context.RequestServices.GetRequiredService<IAuthenticationService>();
+
+    var claims = new List<Claim>
+    {
+        new Claim(ClaimTypes.Name, "test@mail.ru"),
+        new Claim(ClaimTypes.Role, "Administrator"),
+    };
+
+    var jwt = new JwtSecurityToken(
+            issuer: "Issuer",
+            audience: "services",
+            claims: claims,
+            expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+            signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes("mysupersecret_secretsecretsecretkey!123")), SecurityAlgorithms.HmacSha256));
+
+    jwtToken = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+    Console.WriteLine($"Assigned token: {jwtToken}");
+});
+
+app.MapGet("/call", () => {
+    Console.WriteLine("Call");
+
+    using (HttpClient client = new HttpClient()){
+        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, jwtToken) ;
+        var response = client.GetAsync("http://localhost:5000/test").Result;
+
+        Console.WriteLine($"IsSuccess: {response.IsSuccessStatusCode}");
+    }    
+});
+
+app.Run();
