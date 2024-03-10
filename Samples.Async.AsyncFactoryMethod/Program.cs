ca4165f2e4248876050386cec180e0345ce6a1a3
heathk1iff <heathk1iff@outlook.com>
2024-03-10 21:53:01 +0300

    Added samples of async factory method, and initialization pattern

---
+++ Samples.Async.AsyncFactoryMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Application - Start");
+var instance = await AsyncFactoryMethod.CreateAsync();
+Console.WriteLine("Application - End");
+
+public class AsyncFactoryMethod{
+    private AsyncFactoryMethod(){
+        Console.WriteLine("test");
+    }
+
+    private async Task<AsyncFactoryMethod> InitializationAsync(){
+        await Task.Delay(2000);
+        return this;
+    }
+
+    public static Task<AsyncFactoryMethod> CreateAsync(){
+        var result = new AsyncFactoryMethod();
+        Console.WriteLine("CreateAsync");
+        return result.InitializationAsync();
+    } 
+}
---
+++ Samples.Async.InitializationPattern/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+ServiceCollection collection = new ServiceCollection();
+collection.AddTransient<IService, Service>();
+
+using (var provider = collection.BuildServiceProvider()){
+    Console.WriteLine("GetRequiredService<>() - begin");
+    var service = provider.GetRequiredService<IService>();
+    if (service is IAsyncInitialization initializator){
+        await initializator.Initialization;
+    }
+    Console.WriteLine("GetRequiredService<>() - end");
+}
+
+Console.WriteLine("Press any key!");
+Console.ReadLine();
+
+public interface IAsyncInitialization
+{
+    Task Initialization { get; }
+}
+public interface IService
+{
+
+}
+
+public class Service: IService, IAsyncInitialization
+{
+    public Service(){
+        Console.WriteLine("Service() - begin");
+        Initialization = InitializationAsync();
+        Console.WriteLine("Service() - end");
+    }
+
+    public Task Initialization { get; }
+
+    public async Task InitializationAsync()
+    {
+        Console.WriteLine("Delay() - begin");
+        await Task.Delay(2000);
+        Console.WriteLine("Delay() - end");
+    }
+
+}
